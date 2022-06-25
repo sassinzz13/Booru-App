165a2764e0be12a9b31786af8bb5ddaeafdf5b61
its_sassinzz13 <66528388+sassinzz13@users.noreply.github.com>
2022-06-25 13:23:38 +0800

    Serverside Scripts
    contact IzumiFuru for problems relating to servers and if u have tips on how to improve it

---
+++ backEnd/src/routers/server.js
@@ -0,0 +1,10 @@
+//learn https
+/*
+const { createServer } = require('http')
+
+const server = createServer((req,res)=>{
+response.writeHead(200,{'content type': 'text/html'})
+response.write('<h1>hello world</h1>');
+return response.end();
+});
+*/
